9316e7b01dc547a57f3d02c9cf25cebd78632707
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-24 00:47:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const ownerNumber = ['94783747285']
 if (!fs.existsSync(__dirname + "/sessions/creds.json")) {
   if (!config.SESSION_ID)
     return console.log("Please add your session to SESSION_ID env !!");
-  const sessdata = config.SESSION_ID.replace('LUXALGO=', '');
+  const sessdata = config.SESSION_ID.replace("LUXALGO=", '');
   const filer = File.fromURL(`https://mega.nz/file/${sessdata}`);
   filer.download((err, data) => {
     if (err) throw err;
