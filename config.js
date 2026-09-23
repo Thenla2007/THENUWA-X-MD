86d4195ba8d598f9068daacd1e485fee6e7635ab
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-24 02:36:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "LUXALGO=70IDWRBK#GdPnxxNa-8quefn1adKFP6mWAK94B-G9n4cChvv9N9A",
+SESSION_ID: process.env.SESSION_ID || "LUXALGO=6wIQUKQa#RYgcwrIpOPqTYF4EHmQfvCy-hJ51YVoSYnCPAbcUhbI",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
