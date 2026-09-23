250b8d320c02e4c369189d0025d473d2b5384294
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-24 00:57:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,9 +19,9 @@ if (dotenvPath) {
 // Application Configuration Object
 module.exports = {
   // Session and Owner Information
-  SESSION_ID: process.env.SESSION_ID || "HANS-BYTE~tz8UhKBC#y1e0-W46Ung-u6xhwP91fwRZTzTC5gOQgn1ldAVwXio", // Add your session ID here
-  SUDO: process.env.SUDO || "237696900612", // Add your admin/sudo number here
-  OWNER_NUM: process.env.OWNER_NUM || "237696900612", // Add the owner's number here
+  SESSION_ID: process.env.SESSION_ID || "LUXALGO=70IDWRBK#GdPnxxNa-8quefn1adKFP6mWAK94B-G9n4cChvv9N9A", // Add your session ID here
+  SUDO: process.env.SUDO || "94783747285", // Add your admin/sudo number here
+  OWNER_NUM: process.env.OWNER_NUM || "94783747285", // Add the owner's number here
   OWNER_NAME: process.env.OWNER_NAME || "HANS TECH", // Add the owner's name here
   OWNER_EMAIL: process.env.OWNER_EMAIL || "your@email.com", // Add your email address here
   OWNER_LOCATION: process.env.OWNER_LOCATION || "Africa/Douala", // Add your location here
