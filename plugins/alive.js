e76346619b8698d744b927eca674cdbf8954caf8
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-24 15:00:30 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,7 +13,7 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         // Generate system status message
-        const status = `👋 HELLOW...*${pushname || 'User'}* ❤️ I am ALIVE NOW CYBER X THENUVA
+        const status = `👋 HELLOW...*${pushname || 'User'}* 🟢 I am ALIVE NOW CYBER X THENUVA...
 
 
 ✅CYBER THENULA X MD✅
