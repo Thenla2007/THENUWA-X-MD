b52a674f47fa87f0e111990b6db9de7e586eca0e
Thenla2007 <thenulathisan130@gmail.com>
2026-09-25 23:40:47 +0530

    Add files via upload

---
+++ plugins/system.js
@@ -0,0 +1,51 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+cmd({
+    pattern: "system",
+    alias: ["status","botinfo"],
+    desc: "To Check uptime , ram and more.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let status = `
+❖ 𝐒𝐘𝐒𝐓𝐄𝐌 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐓𝐈𝐎𝐍
+
+⏰ UPTIME: ${runtime(process.uptime())}
+📟 RAM USAGE: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+🖥️ HOST NAME: ${os.hostname()}
+👨‍💻 BOT OWNER: THENULA THISAN
+`
+return reply(`${status}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
+
+
+cmd({
+    pattern: "runtime",
+    alias: ["uptime"],
+    desc: "To Check uptime",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let status = `😇𝚁𝚄𝙽𝚃𝙸𝙼𝙴😇:  ${runtime(process.uptime())}`
+
+
+return reply(`${status}`)
+}catch(e){
+console.log(e)
+reply(`${e}`)
+
+}
+})
