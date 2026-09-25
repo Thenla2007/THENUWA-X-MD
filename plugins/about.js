ea0b900401ba9948cdf2b9e67a80bf7e9e9b7493
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-25 09:10:05 +0530

    Create about.js

---
+++ plugins/about.js
@@ -0,0 +1,50 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "about",
+    react: "👑",
+    desc: "get owner dec",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let madeMenu = `╔════════════════════════╗
+   *✨ CYBER THENUVA INFO ✨*
+╚════════════════════════╝
+
+👋 *Hello, ${pushname}!*
+
+╭────────────────────────╮
+  *Developer:*👨‍💻CYBER THENUVA*👨‍💻
+  *Age:* 19 Years Old 🔞
+  *Location:* Personal Hai 😁🙈
+  *Status:* Simple WhatsApp Bot Developer
+╰────────────────────────╯
+
+💡 *Thank you for using my bot!* 
+
+
+> *⚡ Powered By CYBER THENUVA*
+`
+
+await conn.sendMessage(from, {
+    image: { url: config.ALIVE_IMG },
+    caption: madeMenu,
+    contextInfo: {
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: config.NEWSLETTER_JID || '120363403804248705@newsletter',
+            newsletterName: 'CYBER XMD',
+            serverMessageId: 143
+        }
+    }
+}, { quoted: mek })
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
