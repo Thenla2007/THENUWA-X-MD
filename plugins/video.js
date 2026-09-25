217ca90cb93baff6c005b262f38c9abd6bfb6eb0
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-25 19:47:55 +0530

    Update video.js

--- plugins/video.js
+++ plugins/video.js
@@ -52,7 +52,7 @@ cmd({
             };
 
             // Fetch video info from new API
-            const apiUrl = `https://api.apify.com/v2/key-value-stores/MbbSpfx7dYwDJGenr/records/dQw4w9WgXcQ_RickAstley-NeverGonnaGiveYouUp(OfficialVideo)(4KRemaster).mp4?apikey=${encodeURIComponent(videoUrl)}`;
+            const apiUrl = `https://api.giftedtech.web.id/api/download/ytdl?apikey=gifted&url=${encodeURIComponent(videoUrl)}`;
             const response = await fetch(apiUrl);
             const data = await response.json();
 
@@ -126,7 +126,7 @@ cmd({
     }
 
     try {
-        const apiUrl = `https://api.apify.com/v2/key-value-stores/MbbSpfx7dYwDJGenr/records/dQw4w9WgXcQ_RickAstley-NeverGonnaGiveYouUp(OfficialVideo)(4KRemaster).mp4?apikey=${encodeURIComponent(q)}`;
+        const apiUrl = `https://api.apify.com/v2/actor-runs/vhiqDN6KR6fkkb6fm?token=apify_api_o26QUamyP05T5mIlQUZ974yUGLJTed0dScHR${encodeURIComponent(q)}`;
         const response = await fetch(apiUrl);
         const json = await response.json();
 
